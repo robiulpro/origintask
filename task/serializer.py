58041c3f87f6a9ba56468e1406c8723fada0f789
Robiul Hoque <robiul@ajency.in>
2018-06-07 18:48:30 +0530

    added custom fields to in task serializer

--- task/serializer.py
+++ task/serializer.py
@@ -1,14 +1,28 @@
 from rest_framework import serializers
 from .models import Task
+import datetime
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
 
 
 class TaskSerializer(serializers.ModelSerializer):
     created = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
+    expired_until = serializers.SerializerMethodField()
+    is_expired = serializers.SerializerMethodField()
+
+    def get_expired_until(self, task):
+        a = timezone.now()
+        b = task.target_date
+        diff = relativedelta(b, a)
+        return "Expired until %d year %d month %d days %d hours %d minutes" % (diff.years, diff.months, diff.days, diff.hours, diff.minutes)
 
 
-    """ def validate_target_date(self, value):
-        return value """
+    def get_is_expired(self, task):
+        if timezone.now() > task.target_date:
+            return True
+        else:
+            return False
 
     class Meta:
         model = Task
-        fields = ('id', 'title', 'description', 'created', 'updated', 'status', 'created_by', 'assigned_to', 'assigned_on', 'completed_on', 'target_date')
+        fields = ('id', 'title', 'description', 'created', 'updated', 'status', 'created_by', 'assigned_to', 'assigned_on', 'completed_on', 'target_date', 'expired_until', 'is_expired')
